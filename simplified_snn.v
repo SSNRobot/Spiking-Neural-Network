0b742fbb2cecee76c610d9a18f3918b934dc20fa
ronellchakola <ronellchakola@gmail.com>
2022-12-06 12:26:44 -0500

    Basic new structure completed after line 58

--- simplified_snn.v
+++ simplified_snn.v
@@ -13,16 +13,6 @@ module simplified_snn #(
     wire signed [DW + INT_DW - 1 : 0] synapses_results [INPUTNUM - 1 : 0][EXCNUM - 1 : 0];
     wire signed [DW + INT_DW - 1 : 0] after_sum [EXCNUM - 1 : 0];
     wire output_spikes [EXCNUM - 1 : 0];
-    wire spike_inh;
-    
-//        generate
-//        genvar a, b;
-//        for(a = 0; a < EXCNUM; a = a + 1) begin   : loop1
-//            assign spike_inh = spike_inh | output_spikes[a];
-//        end
-//    endgenerate
-
-		assign spike_inh = 0;
     
     reg weights_en;
     always@(posedge clk) begin
@@ -65,5 +55,32 @@ module simplified_snn #(
             );
         end
     endgenerate    
+
+	
+    input_neuron Far_Left (clk,rst,en,Sensor_input[0],Material_type[0],Pre_spike[0]);
+    input_neuron Mid_Left (clk,rst,en,Sensor_input[1],Material_type[1],Pre_spike[1]);
+    input_neuron Mid_Right (clk,rst,en,Sensor_input[2],Material_type[2],Pre_spike[2]);
+    input_neuron Far_Right (clk,rst,en,Sensor_input[3],Material_type[3],Pre_spike[3]);
     
+
+    synapse syn1_1 (.clk(clk), .rst(rst), .en(en), .weights_w(###), .pre_spiking(Pre_spike[0]),.spking_value(synapses_results[0][0]));
+    synapse syn1_2 (.clk(clk), .rst(rst), .en(en), .weights_w(###), .pre_spiking(Pre_spike[0]),.spking_value(synapses_results[0][1]));
+	
+    synapse syn2_1 (.clk(clk), .rst(rst), .en(en), .weights_w(###), .pre_spiking(Pre_spike[1]),.spking_value(synapses_results[1][0]));
+    synapse syn2_2 (.clk(clk), .rst(rst), .en(en), .weights_w(###), .pre_spiking(Pre_spike[1]),.spking_value(synapses_results[1][1]));
+	
+    synapse syn3_1 (.clk(clk), .rst(rst), .en(en), .weights_w(###), .pre_spiking(Pre_spike[2]),.spking_value(synapses_results[2][0]));
+    synapse syn3_2 (.clk(clk), .rst(rst), .en(en), .weights_w(###), .pre_spiking(Pre_spike[2]),.spking_value(synapses_results[2][1]));
+	
+    synapse syn4_1 (.clk(clk), .rst(rst), .en(en), .weights_w(###), .pre_spiking(Pre_spike[3]),.spking_value(synapses_results[3][0]));
+    synapse syn4_2 (.clk(clk), .rst(rst), .en(en), .weights_w(###), .pre_spiking(Pre_spike[3]),.spking_value(synapses_results[3][1]));
+	
+	
+    assign after_sum[0] = synapses_results[0][0] + synapses_results[1][0] + synapses_results[2][0] + synapses_results[3][0];
+    assign after_sum[1] = synapses_results[0][1] + synapses_results[1][1] + synapses_results[2][1] + synapses_results[3][1];
+	
+	
+    exc_neuron Left (clk,rst,en,after_sum[0],Output_spike[0]);
+    exc_neuron Right (clk,rst,en,after_sum[1],Output_spike[1]);
+	
 endmodule
