89fbd2d71ac4df288c0250d73c2cdb85a06fc686
Phillip Salowe <64107928+pjsalowe@users.noreply.github.com>
2023-01-12 16:51:16 -0500

    Update new_neuron.v

--- new_neuron.v
+++ new_neuron.v
@@ -1,59 +1,123 @@
+//  https://github.com/doitdodo/FPGA_Spiking_NN
 
-
-
-module simplified_snn #(
-    parameter DW = 16, INPUTNUM = 4, EXCNUM = 2, INT_DW = 8
+module exc_neuron  #(
+    parameter DW = 16, INT_DW = 8, REFRAC = 5, ENCODE_TIME = 23
 )(
     // Clock, reset and enable
     input wire clk,
     input wire rst,
     input wire en,
-	 input wire[0:3] Sensor_input,
-	 input wire[0:3] Material_type
+    
+    // Input spking values
+    input wire signed [DW + INT_DW - 1 : 0] spiking_value,
+    
+    // Input inh
+    input wire inh,
+    
+    // Output spikes and spike cnt
+    output reg out_spike,
+    output reg [31 : 0] spike_cnt
     );
     
-    wire signed [DW + INT_DW - 1 : 0] synapses_results [INPUTNUM - 1 : 0][EXCNUM - 1 : 0];
-    wire signed [DW + INT_DW - 1 : 0] after_sum [EXCNUM - 1 : 0];
-    wire Output_spike [EXCNUM - 1 : 0];
-    wire spike_inh;
+    localparam inh_value = 24'h3c0000;
+    localparam threshold = 24'h0d0000;
+    localparam reset_v = 24'd0;
 	 
-	 wire Pre_spike[0:3];
-    
-		assign spike_inh = 0;
-    
-    reg weights_en;
+	 reg signed [DW + INT_DW - 1 : 0] potential;
+    reg [3 : 0] refractory_cnt;
+	 
+	 always@(posedge clk) begin
+		if (rst) begin
+			potential <= 0;
+         out_spike <= 0;
+         spike_cnt <= 0;
+		end
+		else if (en) begin
+			if (refractory_cnt == 4'd0) begin
+				potential <= potential + spiking_value;
+				if (inh) begin
+					potential <= potential - inh_value;
+				end
+				
+				if (potential >= threshold) begin
+					potential <= reset_v;			// hard reset potential
+					out_spike <= 1'b1;				// set output spike High
+					spike_cnt <= spike_cnt + 1;	// count total spikes
+				end
+				else
+					out_spike <= 1'b0;				// set output spike Low
+				end
+			else begin
+				potential <= potential;
+				out_spike <= 1'b0;
+				spike_cnt <= spike_cnt;
+			end
+		end
+	 end
+	 
+	 
+	 reg refractory_en;
     always@(posedge clk) begin
-        if(rst) weights_en <= 1'b0;
-        else if (en) weights_en <= en;
+        if(rst) begin 
+            refractory_cnt <= 0;
+            refractory_en <= 0;
+        end
+        else if(en) begin
+            if(refractory_cnt == REFRAC * ENCODE_TIME) begin
+                refractory_cnt <= 0;
+                refractory_en <= 1'b0;
+            end
+            if(potential >= threshold) begin
+                refractory_en <= 1'b1;
+            end
+            else if (refractory_en) begin
+                refractory_cnt <= refractory_cnt + 1;
+            end
+        end
     end
-    wire en_for_initweights;
-    assign en_for_initweights = weights_en ^ en;
-      
 	 
-	 input_neuron Far_Left (clk,rst,en,Sensor_input[0],Material_type[0],Pre_spike[0]);
-    input_neuron Mid_Left (clk,rst,en,Sensor_input[1],Material_type[1],Pre_spike[1]);
-    input_neuron Mid_Right (clk,rst,en,Sensor_input[2],Material_type[2],Pre_spike[2]);
-    input_neuron Far_Right (clk,rst,en,Sensor_input[3],Material_type[3],Pre_spike[3]);
-    
-
-    synapse syn1_1 (.clk(clk), .rst(rst), .en(en), .weights_w(1615585), .pre_spiking(Pre_spike[0]),.spking_value(synapses_results[0][0]));
-    synapse syn1_2 (.clk(clk), .rst(rst), .en(en), .weights_w(592018), .pre_spiking(Pre_spike[0]),.spking_value(synapses_results[0][1]));
-	
-    synapse syn2_1 (.clk(clk), .rst(rst), .en(en), .weights_w(2564138), .pre_spiking(Pre_spike[1]),.spking_value(synapses_results[1][0]));
-    synapse syn2_2 (.clk(clk), .rst(rst), .en(en), .weights_w(-153494), .pre_spiking(Pre_spike[1]),.spking_value(synapses_results[1][1]));
-	
-    synapse syn3_1 (.clk(clk), .rst(rst), .en(en), .weights_w(-88929), .pre_spiking(Pre_spike[2]),.spking_value(synapses_results[2][0]));
-    synapse syn3_2 (.clk(clk), .rst(rst), .en(en), .weights_w(3681132), .pre_spiking(Pre_spike[2]),.spking_value(synapses_results[2][1]));
-	
-    synapse syn4_1 (.clk(clk), .rst(rst), .en(en), .weights_w(568763), .pre_spiking(Pre_spike[3]),.spking_value(synapses_results[3][0]));
-    synapse syn4_2 (.clk(clk), .rst(rst), .en(en), .weights_w(2266863), .pre_spiking(Pre_spike[3]),.spking_value(synapses_results[3][1]));
-	
-	
-    assign after_sum[0] = synapses_results[0][0] + synapses_results[1][0] + synapses_results[2][0] + synapses_results[3][0];
-    assign after_sum[1] = synapses_results[0][1] + synapses_results[1][1] + synapses_results[2][1] + synapses_results[3][1];
-	
-	
-    exc_neuron Left (clk,rst,en,after_sum[0],Output_spike[0]);
-    exc_neuron Right (clk,rst,en,after_sum[1],Output_spike[1]);
-    
 endmodule
+
+
+
+module input_neuron #(
+    parameter ENCODE_TIME = 23, T_WINDOW = 250
+)(
+
+
+    // Clock, reset and enable
+    input wire clk,
+    input wire rst,
+    input wire en,
+    
+	 input wire Sensor_input,
+	 input wire Material_type,
+	 
+	 output wire Pre_spike
+	 
+);
+
+	wire potential;
+	wire spike;
+	
+	assign potential = Sensor_input;
+	
+	always @(posedge clk) 
+	begin
+	
+	if((Material_type > 500) && (Sensor_input < 700)) begin  //change values to 12 bit from 10 bit   and)
+		assign spike <= 1;
+	end
+	else if((Material_type < 500) && (Sensor_input > 275)) begin
+		spike = VCC;
+	end
+	else begin
+		spike <= GND;
+	end
+	
+	
+	assign Pre_spike = spike;
+	
+	end
+endmodule
+
