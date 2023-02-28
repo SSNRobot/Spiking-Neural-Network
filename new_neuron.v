1da3635e169a55642574b3e3f30cdaaf8f569929
Phillip Salowe <64107928+pjsalowe@users.noreply.github.com>
2023-02-28 16:48:08 -0500

    Update new_neuron.v

--- new_neuron.v
+++ new_neuron.v
@@ -21,7 +21,7 @@ module exc_neuron  #(   //output neuron
     
   
 
-	 localparam signed threshold = 16'h0040;  //16 bit hex number
+	 localparam signed threshold = 16'h00F0;//16'h0040;  //16 bit hex number
 	 reg [15 : 0] refractory_cnt = 16'd0;
 //	 reg refractory_en;
 	 
@@ -113,4 +113,76 @@ module input_neuron #(
 	
 	end
 endmodule
+
+
+module hidden_neuron  #(   //output neuron
+	parameter ENCODE_TIME = 23, T_WINDOW = 250
+)(
+    // Clock, reset and enable
+    input wire clk,
+    input wire rst,
+    input wire en,
+	 
+    
+    // Input spking values
+    input wire signed [15 : 0] spiking_value,   //24 bit number
+    
+    
+    // Output spikes and spike cnt
+    output reg out_spike
+
+    );
+    
+  
+
+	 localparam signed threshold = 16'h00F0;//16'h0040;  //16 bit hex number
+	 reg [15 : 0] refractory_cnt = 16'd0;
+//	 reg refractory_en;
+	 
+	 reg signed [15 : 0] potential;    //16 bit number
+	 
+	 always@(posedge clk) begin
+		if (rst) begin
+			potential <= 0;
+         out_spike <= 0;
+//         refractory_cnt <= 0;
+//         refractory_en <= 0;
+
+		end
+		else if (en) begin
+		
+			potential <= potential + spiking_value;
+				
+		end
+		
+		if(en) begin
+            if(refractory_cnt == 16'd0) begin
+                potential <= potential + spiking_value;
+                
+                if((potential >= threshold) ) begin//&& potential >= 0) begin
+                    potential <= 16'b0;
+                    out_spike <= 1'b1;
+//         		     refractory_en <= 1'b1;
+                end
+                else out_spike <= 1'b0;
+            end
+            else begin
+                potential <= potential;         
+                out_spike <= 1'b0; 
+            end
+//				if(refractory_cnt == 50) begin   //100 clock cycle delay, which makes 2us delay
+//                refractory_cnt <= 0;
+//                refractory_en <= 1'b0;
+//            end
+//            else if (refractory_en) begin
+//                refractory_cnt <= refractory_cnt + 1;
+//            end
+        end
+		
+	 end
+	 
+	 
+endmodule
+
+
 
